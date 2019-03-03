a0e86e6d8206159ccbd978c83f33b8040de71dfd
omerio9 <omegambadu02@gmail.com>
2019-03-03 22:19:17 +0200

    test_cases_Updated

---
+++ src/test/java/AccountDaoTest.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void insert() {
+   fail("not tested yet");
+    }
+}
