e11c0d362ee5e418efd55f9509992d8a29afd2c2
omerio9 <omegambadu02@gmail.com>
2019-03-03 22:17:07 +0200

    test_cases_Updated

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,15 +1,25 @@
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import static org.junit.Assert.*;
 
 public class AccountTest extends TestCase {
 
-    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Before
+    public void setUp() throws Exception {
 
-             AccountService accountService = applicationContext.getBean("accountService",AccountService.class);
+    }
 
+    @After
+    public void tearDown()throws Exception{
+
+    }
              @Test
                     public void test_Acc_balance() {
+                 AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+                 AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
                 System.out.println("Account 1 balance :" + accountService.getAccount(String.valueOf(1234)).getBalance());
                 System.out.println("Account 2 balance :" + accountService.getAccount(String.valueOf(2456)).getBalance());
             }
